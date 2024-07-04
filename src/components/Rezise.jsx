3c45659c59e8ed70dc10a25f7810cf27a8574dd3
Nahuel Aguirre MacBook Pro <nahuelaguirre@Nahuels-MacBook-Pro.local>
2024-07-04 14:09:48 -0600

    SE hiceiron ajustes pero no tira un error

--- src/components/Rezise.jsx
+++ src/components/Rezise.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
 
+
 function Resizable() {
   const [display, setDisplay] = useState(true);
   const [width, setWidth] = useState(600);
@@ -9,17 +10,17 @@ function Resizable() {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1
   };
   return (
     <div className="slider-container">
       <h2> Resizable Collapsible </h2>
-      <button className="button" onClick={() => setWidth(width + 100)}>
+      <button className="button" onClick={() => setWidth(width + 50)}>
         {" "}
         increase{" "}
       </button>
-      <button className="button" onClick={() => setWidth(width - 100)}>
+      <button className="button" onClick={() => setWidth(width - 50)}>
         {" "}
         decrease{" "}
       </button>
@@ -34,26 +35,60 @@ function Resizable() {
         }}
       >
         <Slider {...settings}>
-          <div>
-            <h3>Primero</h3>
+          <div className="card">
+            <img src="https://cdn.stocksnap.io/img-thumbs/960w/isolated-leaf_EP1B8CMRC6.jpg" alt=""/>
+            <div className="card-body">
+              <h3>Slick</h3>
+              <p>Estoy usando React</p>
+            </div>
           </div>
-          <div>
-            <h3>2</h3>
+          <div className="card">
+           <img src="https://cdn.stocksnap.io/img-thumbs/960w/white-ibis_Q6A4HCTY9W.jpg" alt=""/>
+           <div className="card-body">
+           <h3>Slick</h3>
+           <p>Estoy usando React</p>
+           </div>
           </div>
-          <div>
-            <h3>3</h3>
+          <div className="card">
+           <img src="https://cdn.stocksnap.io/img-thumbs/960w/astro-star%20trails_8UPLJEGABH.jpg" alt=""/>
+           <div className="card-body">
+           <h3>Slick</h3>
+           <p>Estoy usando React</p>
+           </div>
           </div>
-          <div>
-            <h3>4</h3>
+          <div className="card">
+           <img src="https://cdn.stocksnap.io/img-thumbs/960w/autumn-forest_KCZNDRYSHE.jpg" alt=""/>
+           <div className="card-body">
+           <h3>Slick</h3>
+           <p>Estoy usando React</p>
+           </div>
           </div>
-          <div>
-            <h3>5</h3>
+          <div className="card">
+           <img src="https://cdn.stocksnap.io/img-thumbs/960w/forest-flowers_TQ6J8JFQXS.jpg" alt=""/>
+           <div className="card-body">
+           <h3>Slick</h3>
+           <p>Estoy usando React</p>
+           </div>
           </div>
-          <div>
-            <h3>6</h3>
+          <div className="card">
+           <img src="https://cdn.stocksnap.io/img-thumbs/960w/umbrella-rain_KIJJLUCS7S.jpg" alt=""/>
+           <div className="card-body">
+           <h3>Slick</h3>
+           <p>Estoy usando React</p>
+           </div>
           </div>
         </Slider>
       </div>
+
+      <div className="Resizable">
+            <Slider {...settings}>
+                {images.map((image,index)=>(
+                    <div key={index}>
+                        <img src={image} alt={`Slide ${index}`}/>
+                    </div>
+                ))}
+            </Slider>
+        </div>
     </div>
   );
 }
